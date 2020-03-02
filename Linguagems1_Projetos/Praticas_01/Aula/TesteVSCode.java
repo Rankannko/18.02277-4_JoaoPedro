c7a886e6820f10586e1d1451a2181e94eb1d2870
JOAO  PEDRO DE PADUA SANTORO AZEVEDO <18.02277-4@maua.br>
2020-03-02 13:05:34 -0300

    Aula 2 - Java

---
+++ Linguagems1_Projetos/Praticas_01/Aula/TesteVSCode.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+class TesteVSCode{
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(system.in);
+        System.out.println("Ola mundo");
+        String nome;
+        nome=scanner.nextLine();
+        System.out.println(nome);
+    }
+}
