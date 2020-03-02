5ad0481da879ea89d091bdc36e8fe54fe1de9a0a
JOAO  PEDRO DE PADUA SANTORO AZEVEDO <18.02277-4@maua.br>
2020-03-02 11:31:41 -0300

    Arquivos criados

---
+++ Linguagems1_Projetos/Praticas_01/Aula/MeuPrograma.java
@@ -0,0 +1,8 @@
+class MeuPrograma{
+    public static void main(String[] args){
+        //Mensagem de saida do sistema
+        System.out.println("Minha Primeira aplicação Java!!");
+        System.out.println("Ola mundo!");
+        System.out.println("Modo Conan on!");
+    }
+}
